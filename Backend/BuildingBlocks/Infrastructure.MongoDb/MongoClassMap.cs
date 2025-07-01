e0eca9524a714a4d60bc1adce3774b2fe15784bd
mtddev <mtddev2004@gmail.com>
2025-07-01 23:18:31 +1000

    feat: added mongodb classmap and idempotency

---
+++ Backend/BuildingBlocks/Infrastructure.MongoDb/MongoClassMap.cs
@@ -0,0 +1,8 @@
+using MongoDB.Bson.Serialization;
+
+namespace Infrastructure.MongoDb;
+
+public abstract class MongoClassMap<TDocument>
+{
+	public abstract void RegisterClassMap(BsonClassMap<TDocument> classMap);
+}
---
+++ Backend/BuildingBlocks/Infrastructure.MongoDb/MongoClassMapHelper.cs
@@ -0,0 +1,61 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson.Serialization;
+
+namespace Infrastructure.MongoDb;
+
+public static class MongoClassMapHelper
+{
+	public static void RegisterMongoClassMaps(this IHost app, params Assembly[] assemblies)
+	{
+		assemblies = assemblies.Union(new List<Assembly> { typeof(MongoClassMapHelper).Assembly }).ToArray();
+
+		foreach (var assembly in assemblies)
+		foreach (var classMap in assembly.GetExportedTypes())
+			if (classMap.IsClass && !classMap.IsAbstract)
+			{
+				var documentType = GetDocumentType(classMap);
+
+				if (documentType != null)
+				{
+					using var scope = app.Services.CreateScope();
+					var services = scope.ServiceProvider;
+
+					var logger = services.GetRequiredService<ILogger<MongoClassMapRegister>>();
+
+					try
+					{
+						var classMapInstance = Activator.CreateInstance(classMap);
+						var registerClassMapMethod =
+							classMap.GetMethod("RegisterClassMap", BindingFlags.Instance | BindingFlags.Public);
+
+						var bsonClassMapType = typeof(BsonClassMap<>).MakeGenericType(documentType);
+						var bsonClassMap = Activator.CreateInstance(bsonClassMapType) as BsonClassMap;
+
+						registerClassMapMethod!.Invoke(classMapInstance, new object[] { bsonClassMap! });
+
+						BsonClassMap.RegisterClassMap(bsonClassMap);
+					}
+					catch (Exception ex)
+					{
+						logger.LogError(ex, "Failed to register class map of {DocumentType}", documentType.Name);
+					}
+				}
+			}
+	}
+
+	private static Type? GetDocumentType(Type classMapType)
+	{
+		while (!classMapType.IsGenericType || classMapType.GetGenericTypeDefinition() != typeof(MongoClassMap<>))
+			if (classMapType.BaseType != null)
+				classMapType = classMapType.BaseType;
+			else
+				return null;
+
+		return classMapType.GetGenericArguments()[0];
+	}
+
+	private class MongoClassMapRegister { }
+}
