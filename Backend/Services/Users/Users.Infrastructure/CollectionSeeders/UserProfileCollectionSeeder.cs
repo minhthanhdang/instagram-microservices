bfcc7c4cb512dea75277bc8a5972c765a7ee0059
mtddev <mtddev2004@gmail.com>
2025-07-05 21:35:55 +1000

    added mongo collection seeder

---
+++ Backend/Services/Users/Users.Infrastructure/CollectionSeeders/UserProfileCollectionSeeder.cs
@@ -0,0 +1,24 @@
+using Infrastructure.MongoDb;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Users.Domain.Models;
+
+namespace Users.Infrastructure.CollectionSeeders;
+
+public class UserProfileCollectionSeeder : MongoCollectionSeeder<UserProfile>
+{
+	public override async Task SeedAsync(IServiceProvider services, IMongoCollection<UserProfile> collection,
+		ILogger<IMongoCollection<UserProfile>> logger)
+	{
+		var indexBuilder = Builders<UserProfile>.IndexKeys;
+
+		var indexes = new List<CreateIndexModel<UserProfile>>
+		{
+			new(indexBuilder.Ascending(nameof(UserProfile.Username)),
+				new CreateIndexOptions { Unique = true, Sparse = true }
+			)
+		};
+
+		await collection.Indexes.CreateManyAsync(indexes);
+	}
+}
